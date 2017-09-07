7c82ebfa6e00d993eedbe66c12700ba90289ca5d
AleksAttanasio <aleks.attanasio@gmail.com>
2017-09-07 19:21:42 +0200

    Added first GUI, Add function working

---
+++ CustomerManager/mainForm.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace CustomerManager
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.customersGrid = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.surnameTxt = new System.Windows.Forms.TextBox();
+            this.emailTxt = new System.Windows.Forms.TextBox();
+            this.phoneTxt = new System.Windows.Forms.TextBox();
+            this.companyTxt = new System.Windows.Forms.TextBox();
+            this.cityTxt = new System.Windows.Forms.TextBox();
+            this.addressTxt = new System.Windows.Forms.TextBox();
+            this.refreeTxt = new System.Windows.Forms.TextBox();
+            this.websiteTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.customersGrid)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // customersGrid
+            // 
+            this.customersGrid.AllowUserToAddRows = false;
+            this.customersGrid.AllowUserToDeleteRows = false;
+            this.customersGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.customersGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.customersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.customersGrid.Location = new System.Drawing.Point(12, 380);
+            this.customersGrid.Name = "customersGrid";
+            this.customersGrid.RowTemplate.Height = 24;
+            this.customersGrid.Size = new System.Drawing.Size(988, 296);
+            this.customersGrid.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.websiteTxt);
+            this.groupBox1.Controls.Add(this.refreeTxt);
+            this.groupBox1.Controls.Add(this.addressTxt);
+            this.groupBox1.Controls.Add(this.cityTxt);
+            this.groupBox1.Controls.Add(this.companyTxt);
+            this.groupBox1.Controls.Add(this.phoneTxt);
+            this.groupBox1.Controls.Add(this.emailTxt);
+            this.groupBox1.Controls.Add(this.surnameTxt);
+            this.groupBox1.Controls.Add(this.nameTxt);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 354);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Scheda Nuovo Cliente";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cognome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefono:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Azienda:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Città:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Indirizzo:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 245);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Referente:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(15, 275);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Website:";
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(84, 32);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(344, 22);
+            this.nameTxt.TabIndex = 9;
+            // 
+            // surnameTxt
+            // 
+            this.surnameTxt.Location = new System.Drawing.Point(85, 62);
+            this.surnameTxt.Name = "surnameTxt";
+            this.surnameTxt.Size = new System.Drawing.Size(344, 22);
+            this.surnameTxt.TabIndex = 10;
+            // 
+            // emailTxt
+            // 
+            this.emailTxt.Location = new System.Drawing.Point(84, 92);
+            this.emailTxt.Name = "emailTxt";
+            this.emailTxt.Size = new System.Drawing.Size(344, 22);
+            this.emailTxt.TabIndex = 11;
+            // 
+            // phoneTxt
+            // 
+            this.phoneTxt.Location = new System.Drawing.Point(84, 122);
+            this.phoneTxt.Name = "phoneTxt";
+            this.phoneTxt.Size = new System.Drawing.Size(122, 22);
+            this.phoneTxt.TabIndex = 12;
+            // 
+            // companyTxt
+            // 
+            this.companyTxt.Location = new System.Drawing.Point(84, 152);
+            this.companyTxt.Name = "companyTxt";
+            this.companyTxt.Size = new System.Drawing.Size(344, 22);
+            this.companyTxt.TabIndex = 13;
+            // 
+            // cityTxt
+            // 
+            this.cityTxt.Location = new System.Drawing.Point(84, 182);
+            this.cityTxt.Name = "cityTxt";
+            this.cityTxt.Size = new System.Drawing.Size(344, 22);
+            this.cityTxt.TabIndex = 14;
+            // 
+            // addressTxt
+            // 
+            this.addressTxt.Location = new System.Drawing.Point(84, 212);
+            this.addressTxt.Name = "addressTxt";
+            this.addressTxt.Size = new System.Drawing.Size(344, 22);
+            this.addressTxt.TabIndex = 15;
+            // 
+            // refreeTxt
+            // 
+            this.refreeTxt.Location = new System.Drawing.Point(85, 242);
+            this.refreeTxt.Name = "refreeTxt";
+            this.refreeTxt.Size = new System.Drawing.Size(344, 22);
+            this.refreeTxt.TabIndex = 16;
+            // 
+            // websiteTxt
+            // 
+            this.websiteTxt.Location = new System.Drawing.Point(84, 272);
+            this.websiteTxt.Name = "websiteTxt";
+            this.websiteTxt.Size = new System.Drawing.Size(344, 22);
+            this.websiteTxt.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 36);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Aggiungi Cliente";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 680);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.customersGrid);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1030, 727);
+            this.Name = "mainForm";
+            this.Text = "Customer Manager";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.customersGrid)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView customersGrid;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox websiteTxt;
+        private System.Windows.Forms.TextBox refreeTxt;
+        private System.Windows.Forms.TextBox addressTxt;
+        private System.Windows.Forms.TextBox cityTxt;
+        private System.Windows.Forms.TextBox companyTxt;
+        private System.Windows.Forms.TextBox phoneTxt;
+        private System.Windows.Forms.TextBox emailTxt;
+        private System.Windows.Forms.TextBox surnameTxt;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CustomerManager/mainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CustomerManager
+{
+    public partial class mainForm : Form
+    {
+        SqlConnection connection;
+        string connectionString;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["CustomerManager.Properties.Settings.CustomerDatabaseConnectionString"].ConnectionString;
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            UpdateMainForm();
+        }
+
+        public void UpdateMainForm()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Customers", connection))
+            {
+                DataTable peopleTable = new DataTable();
+                adapter.Fill(peopleTable);
+
+                customersGrid.DataSource = peopleTable;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Customers VALUES (@name, @surname, @email, @phone, @company, @city, @address, @refree, @website)";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@name", nameTxt.Text);
+                command.Parameters.AddWithValue("@surname", surnameTxt.Text);
+                command.Parameters.AddWithValue("@email", emailTxt.Text);
+                command.Parameters.AddWithValue("@phone", phoneTxt.Text);
+                command.Parameters.AddWithValue("@company", companyTxt.Text);
+                command.Parameters.AddWithValue("@city", cityTxt.Text);
+                command.Parameters.AddWithValue("@address", addressTxt.Text);
+                command.Parameters.AddWithValue("@refree", refreeTxt.Text);
+                command.Parameters.AddWithValue("@website", websiteTxt.Text);
+
+                command.ExecuteNonQuery();
+            }
+
+            UpdateMainForm();
+
+        }
+    }
+}
